1ea2e231290241f54888c7f8c2b86ae517016b72
Anderson Correia de Souza <anderson_souza33@outlook.com.br>
2021-05-27 11:54:29 -0300

    feat: animação do sobre

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+/* let sobre = []
+sobre.push(titulo);
+sobre.push(estudo);
+sobre.push(passatempo); */
+
+function sobre(){
+    setTimeout(aparecerTitulo, 700);
+    setTimeout(aparecerEstudo, 1400);
+    setTimeout(aparecerPassaTempo, 2100);
+}
+
+function aparecerTitulo(){
+    let titulo = window.document.querySelector('h1');
+    titulo.style.display = 'block';
+}
+
+function aparecerEstudo(){
+    let resumo = window.document.querySelector('#resumo');
+    resumo.style.display = 'block';
+}
+
+function aparecerPassaTempo(){
+    let passatempo = window.document.querySelector('#passatempo');
+    passatempo.style.display = 'block';
+}
